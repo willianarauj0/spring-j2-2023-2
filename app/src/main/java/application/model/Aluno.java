1d8fc58282ee10249210b7f5761dc8fc017e1b3f
marcoweb <marcocarvalho.web@gmail.com>
2023-10-17 19:15:18 +0000

    Repository Aluno

--- app/src/main/java/application/model/Aluno.java
+++ app/src/main/java/application/model/Aluno.java
@@ -1,6 +1,16 @@
 package application.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="alunos")
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private int idade;
---
+++ app/src/main/java/application/model/AlunoRepository.java
@@ -0,0 +1,7 @@
+package application.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlunoRepository extends CrudRepository<Aluno, Integer> {
+    
+}
