97577be50e79b3f78d6c5c479cb2e0f9e0db7a4c
marcoweb <marcocarvalho.web@gmail.com>
2023-10-17 19:45:11 +0000

    Add Repo Controller Aluno

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -1,11 +1,17 @@
 package application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.model.AlunoRepository;
 
 @Controller
 @RequestMapping("/aluno")
 public class AlunoController {
+    @Autowired
+    private AlunoRepository alunoRepo;
+
     @RequestMapping("/list")
     public String list() {
         return "/aluno/list";
