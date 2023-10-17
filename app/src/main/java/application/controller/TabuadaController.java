2822e25722f9ba8572e488c9d519ce370e844660
marcoweb <marcocarvalho.web@gmail.com>
2023-10-17 17:26:43 +0000

    JSP

--- app/src/main/java/application/controller/TabuadaController.java
+++ app/src/main/java/application/controller/TabuadaController.java
@@ -13,7 +13,13 @@ public class TabuadaController {
         @RequestParam("numero") int num,
         Model ui
     ) {
+        int[] resultado = new int[10];
+        for(int pos = 0;pos < 10;pos++) {
+            resultado[pos] = (pos + 1) * num;
+        }
+
         ui.addAttribute("valor", num);
+        ui.addAttribute("resultado", resultado);
         return "tabuada";
     }
 }
